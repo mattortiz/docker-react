dcc1ad56cfb8662a8c53dfe2bfbf382ca187836a
Matt Ortiz <matt@mattortiz.com>
2019-05-04 12:11:04 -0700

    small change

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi there!</p>
+        <p>Hi there! (changed)</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React (was changed)
+          Learn React
         </a>
       </header>
     </div>
